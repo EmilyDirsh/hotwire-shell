11327e742020b5529aef6fb16d51563060298ebd
cgwalters <cgwalters@79f8d753-db37-0410-980a-393e3d9e3e3e>
2007-11-08 15:57:24 +0000

    Switch proc from taking -u to -a.

--- hotwire/builtins/proc.py
+++ hotwire/builtins/proc.py
@@ -9,11 +9,11 @@ class PsBuiltin(Builtin):
         super(PsBuiltin, self).__init__('proc',
                                         output=Process,
                                         idempotent=True,
-                                        options=[['-u', '--user'],],                                        
+                                        options=[['-a', '--all'],],                                        
                                         threaded=True)
 
     def execute(self, context, options=[]):
-        myself_only = '-u' in options        
+        myself_only = '-a' not in options        
         pm = ProcessManager.getInstance()
         if not myself_only:
             for proc in pm.get_processes():
