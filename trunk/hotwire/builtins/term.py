0438072713d9b80a334e7a01e99485752ff58124
cgwalters <cgwalters@79f8d753-db37-0410-980a-393e3d9e3e3e>
2007-11-16 15:11:49 +0000

    Add -w option to term.  Change various commands like sudo, su to use -w by default.
    When a terminal closes and a single window is open, don't automatically
    close the tab/window.

--- trunk/hotwire/builtins/term.py
+++ trunk/hotwire/builtins/term.py
@@ -4,10 +4,11 @@ class TermBuiltin(Builtin):
     """Execute a system command in a new terminal."""
     def __init__(self):
         super(TermBuiltin, self).__init__('term', nostatus=True,
-                                          parseargs='str')
+                                          parseargs='str-shquoted',
+                                          options=[['-w', '--window']])
 
-    def execute(self, context, arg):
-        context.hotwire.open_term(context.cwd, context.pipeline, arg)
+    def execute(self, context, arg, options=[]):
+        context.hotwire.open_term(context.cwd, context.pipeline, arg, window=('-w' in options))
         return []
         
 BuiltinRegistry.getInstance().register(TermBuiltin())
