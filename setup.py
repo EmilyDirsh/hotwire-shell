be04426f9f4ba0a62901a1538296223779d1ef70
cgwalters <cgwalters@79f8d753-db37-0410-980a-393e3d9e3e3e>
2008-06-29 22:59:52 +0000

    Fix invocation of gtk-update-icon-cache
    This makes things work on non-%post package systems

--- setup.py
+++ setup.py
@@ -103,7 +103,7 @@ class HotInstall(install):
         if os.name == 'posix':                       
             if self.root is None:
                 print "Running gtk-update-icon-cache"
-                subprocess.call(['gtk-update-icon-cache', os.path.join(self.install_data, 'icons')])
+                subprocess.call(['gtk-update-icon-cache', os.path.join(self.install_data, 'share', 'icons', 'hicolor')])
 kwargs['cmdclass']['install'] = HotInstall                    
 
 setup(name='hotwire',
